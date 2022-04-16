94443e31e9ade21b0425796adf7852bcd97d97dc
Kamil Kuziora <kam.kuziora@gmail.com>
2022-04-17 00:57:05 +0200

    dmenu updated new script etc.

--- dwmblocks/blocks.def.h
+++ dwmblocks/blocks.def.h
@@ -1,13 +1,13 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", "dwm-brightness", 60, 7},
-	{"", "dwm-volume", 0, 6 }, 
-	{" ", "dwm-cpu", 2, 5},
-	{"﬙ ", "free -m | awk '/^Mem:/ {print $3\" MiB\"}'", 2, 4},
-	{" ", "upower -i /org/freedesktop/UPower/devices/battery_BAT0 | awk '/percentage/ {print $2}'", 60, 	3},
-	{" ", "date +%d-%m-%Y", 60, 	2},
-	{" ", "date +%H:%M", 	60,		1},
+	{"", "dwm-brightness",      60,     7},
+	{"", "dwm-volume",          30,     6}, 
+	{"", "dwm-cpu",             2,      5},
+	{"", "dwm-memory",          2,      4},
+	{"", "dwm-battery",         60,     3},
+	{"", "dwm-date",            60, 	2},
+	{"", "dwm-time", 	        1,		1},
 	
 };
 
