fc189d9dcc6972dbfd98555fb14b630c6a60d018
Kamil Kuziora <kam.kuziora@gmail.com>
2022-04-17 10:07:36 +0200

    new version of dwmblocks

---
+++ dwmblocks-async/config.h
@@ -0,0 +1,13 @@
+#define CMDLENGTH 60
+#define DELIMITER "  "
+#define CLICKABLE_BLOCKS
+
+const Block blocks[] = {
+	BLOCK("dwm-brightness",        10, 7),
+	BLOCK("dwm-volume",            10, 6),
+	BLOCK("dwm-cpu",                2, 5),
+	BLOCK("dwm-memory",             2, 4),
+	BLOCK("dwm-battery",           60, 3),
+	BLOCK("dwm-date",              60, 2),
+	BLOCK("dwm-time",               5, 27),
+};
