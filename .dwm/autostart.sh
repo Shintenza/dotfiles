f17ef2430404128977006c9e17b30bcea1e897ad
Kamil Kuziora <kam.kuziora@gmail.com>
2022-04-18 17:32:25 +0200

    fixed scripts

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -1,12 +1,10 @@
 #!/bin/zsh
 
+. $HOME/.zshrc
 wallpaper &
-# ./.config/polybar/launch.sh &
 setxkbmap pl &
- redshift -l 50.60705:22.10381 & 
-# xautolock -time 10 -locker 'i3lock-fancy' &│
+redshift -l 50.60705:22.10381 & 
 /usr/bin/lxpolkit &
 picom -b &
 sleep 2; dwmblocks &
 xautolock -time 10 -locker "betterlockscreen -l blur" -detectsleep
-
--- dwmblocks-async/config.h
+++ dwmblocks-async/config.h
@@ -1,6 +1,5 @@
 #define CMDLENGTH 60
 #define DELIMITER "  "
-#define CLICKABLE_BLOCKS
 
 const Block blocks[] = {
 	BLOCK("dwm-brightness",        10, 7),
@@ -9,5 +8,5 @@ const Block blocks[] = {
 	BLOCK("dwm-memory",             2, 4),
 	BLOCK("dwm-battery",           60, 3),
 	BLOCK("dwm-date",              60, 2),
-	BLOCK("dwm-time",               5, 27),
+	BLOCK("dwm-time",               5, 1),
 };
