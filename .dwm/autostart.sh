7bf8d203ea8f6fa5c6d6262cb3031dadf51f82b1
Kamil <49399623+Shintenza@users.noreply.github.com>
2022-08-31 17:51:43 +0200

    fixed xidlehook command syntax

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -8,8 +8,7 @@ redshift -l 50.60705:22.10381 &
 /usr/bin/lxpolkit &
 picom -b --experimental-backends &
 dwmblocks &
-xidlehook --not-when-fullscreen --not-when-audio \ 
-    --timer 580 'xbacklight -set 5' \
-    --timer 20 'xbacklight -set 100; betterlockscreen -l' \
-    --timer 600 'systemctl suspend' \
-    ''
+xidlehook --not-when-fullscreen --not-when-audio \
+    --timer 580 'xbacklight -set 5' 'xbacklight -set 100' \
+    --timer 20 'xbacklight -set 100; betterlockscreen -l' '' \
+    --timer 600 'systemctl suspend' ''\
